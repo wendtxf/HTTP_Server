88a63427b3ecb12c62c70052844773301143b5aa
Wendell Freitas <94016475+wentxf@users.noreply.github.com>
2023-11-01 16:16:28 -0300

    Add files via upload

---
+++ HTTP_Server.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    char tree;
+
+    system("clear");
+    printf("===========");
+    printf(" HTTP Server");
+    printf(" ===========\n\n");
+    printf("Listar Árvore de Diretórios? (S/N): ");
+    scanf("%s",&tree);
+    printf("\n");
+
+    if(tree == 's' || tree == 'S'){
+        system("clear");
+        printf("===========");
+        printf(" HTTP Server");
+        printf(" ===========\n\n");
+
+        printf("Inicialização Concluída!\n\n");
+        system("hostname; hostname -I; echo Service: 8000; echo");
+        printf("Listando...\n");
+
+        system("tree /mnt/c/Users/'Wendell Freitas'/Nextcloud/Ferramentas");
+    }else if(tree == 'n' || tree == 'N'){
+        system("clear");
+        printf("===========");
+        printf(" HTTP Server");
+        printf(" ===========\n\n");
+
+        printf("Inicialização Concluída!\n\n");
+        system("hostname; hostname -I | cut -f1 -d' '; echo Service: 8000; echo");
+    }else{
+        system("clear");
+        printf("===========");
+        printf(" HTTP Server");
+        printf(" ===========\n\n");
+
+        printf("Inicialização Concluída!\n\n");
+        system("hostname; hostname -I; echo Service: 8000; echo");
+    }
+
+  //   ip -4 addr show wifi0 | grep -oP '(?<=inet/s)/d+(/./d+){3}'");
+
+    system("cd /mnt/c/Users/'Wendell Freitas'/Nextcloud/Ferramentas; python3 -m http.server");
+    system("clear");
+}
